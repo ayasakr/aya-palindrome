edf2b5ac3a1fe27e634e48f5f564ef3b5f23a0f0
Aya Sakr <ayasakr91@gmail.com>
2018-07-09 14:24:12 +0300

    Finish palindrome without empty space

--- bundle.js
+++ bundle.js
@@ -1,16 +1,82 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-let Phrase = require("aya-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
+module.exports = Phrase;
 
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-},{"aya-palindrome":2}],2:[function(require,module,exports){
+// Adds 'blank' to all strings.
+String.prototype.blank = function() {
+  return (/^(\s*)$/).test(this);
+}
+
+//Adds 'last' to all arrays.
+Array.prototype.last = function() {
+  return this.slice(-1);
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns the letters in a string
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    if(this.content){
+      console.log("there is content" + this.content);
+      return (this.processedContent() === this.processedContent().reverse());
+    }
+    else
+    {
+      console.log("no content");
+      return false;
+    }
+
+  }
+
+  // Makes the phrase LOUDER.
+  this.louder = function louder() {
+    return this.content.toUpperCase();
+  };
+}
+
+},{}],2:[function(require,module,exports){
+let Phrase = require("aya-palindrome");
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"\'${phrase.content}\'`.bold() + ` is a palindrome!\"`;
+  } else {
+    palindromeResult.innerHTML = `"\'${phrase.content}\'`.bold() + ` is a not a palindrome.\"`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
+
+},{"aya-palindrome":3}],3:[function(require,module,exports){
 module.exports = Phrase;
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
@@ -46,7 +112,7 @@ function Phrase(content) {
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    return this.content && this.processedContent() === this.processedContent().reverse();
   }
 
   // Makes the phrase LOUDER.
@@ -55,4 +121,4 @@ function Phrase(content) {
   };
 }
 
-},{}]},{},[1]);
+},{}]},{},[1,2]);
--- test/test.js
+++ test/test.js
@@ -10,6 +10,11 @@ describe("Phrase", function() {
       assert(!nonPalindrome.palindrome());
     });
 
+    it("should return false for a an empty string", function() {
+      let nonPalindrome = new Phrase("");
+      assert(!nonPalindrome.palindrome());
+    });
+
     it("should return true for a plain palindrome", function() {
       let plainPalindrome = new Phrase("racecar");
       assert(plainPalindrome.palindrome());
